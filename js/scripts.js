a0597729ee74771084fefb9ef77b6269f42a407a
telpuhova <telpuhova@gmail.com>
2017-12-11 09:56:04 -0800

    add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@ function Place(location, month, year, notes) {
   this.year = year;
   this.notes = notes;
 }
+
+//ui
+$(document).ready(function() {
+  $("#formPlaces").submit(function() {
+    event.preventDefault();
+    var location = $("#location").val();
+    var month = $("#month").val();
+    var year = $("#year").val();
+    var notes = $("#notes").val();
+
+    var place = new Place(location, month, year, notes);
+
+    $("#result-list").append("<li>" + location + "</li>");
+    $("#contact-display").empty();
+    $("#contact-display").append(location + "<br>" + month + "<br>" + year + "<br>" + notes)
+  });
+});
